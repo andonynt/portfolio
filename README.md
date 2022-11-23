cca9887641be32ddc6a58ea0b7ba355f780c314c
Andony Nuñez <andonynunezt@gmail.com>
2022-11-23 17:54:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Andony Nunez - Frontend Developer
+
+Hello and welcome to the code for my personal site! Hope you like it.
+
+- Built with [Astro](https://astro.build/).
+
+View my LinkedIn profile at https://www.linkedin.com/in/andonynunezt/.
---
+++ src/static/projects.js
@@ -0,0 +1,105 @@
+export default [
+  {
+    title: 'Linkeefy',
+    text: 'Linkeefy allows you to share your favorite links (social media, personal links, etc) at one place.',
+    appImage: 'linkeefy.png',
+    technologies: [
+      {
+        icon: 'react.svg',
+        name: 'React'
+      },
+      {
+        icon: 'firebase.svg',
+        name: 'Firebase'
+      },
+      {
+        icon: 'tailwindcss.svg',
+        name: 'Tailwind CSS'
+      },
+      {
+        icon: 'reactrouter.svg',
+        name: 'React Router'
+      },
+      {
+        icon: 'zustand.png',
+        name: 'Zustand'
+      },
+    ],
+    links: [
+      {
+        url: 'https://linkeefy-app.firebaseapp.com/',
+        text: 'live site',
+      },
+      {
+        url: 'https://github.com/andonynt/linkeefy-app',
+        text: 'repo',
+      },
+    ]
+  },
+  {
+    title: 'Free Meal',
+    text: 'Find and save a large variety of meals in one place.',
+    appImage: 'free-meal.png',
+    technologies: [
+      {
+        icon: 'react.svg',
+        name: 'React'
+      },
+      {
+        icon: 'firebase.svg',
+        name: 'Firebase'
+      },
+      {
+        icon: 'tailwindcss.svg',
+        name: 'Tailwind CSS'
+      },
+      {
+        icon: 'reactrouter.svg',
+        name: 'React Router'
+      },
+    ],
+    links: [
+      {
+        url: 'https://mealsdb-react-app.web.app/',
+        text: 'live site',
+      },
+      {
+        url: 'https://github.com/andonynt/linkeefy-app',
+        text: 'repo',
+      },
+    ]
+  },
+  {
+    title: 'Wallet tracker',
+    text: 'Track your incomes and expenses by knowing where your money is going.',
+    appImage: 'expense.png',
+    technologies: [
+      {
+        icon: 'react.svg',
+        name: 'React'
+      },
+      {
+        icon: 'awsamplify.svg',
+        name: 'Amplify'
+      },
+      {
+        icon: 'amazondynamodb.svg',
+        name: 'DynamoDB'
+      },
+      {
+        icon: 'zustand.png',
+        name: 'Zustand'
+      },
+      {
+        icon: 'd3dotjs.svg',
+        name: 'D3.js'
+      }
+    ],
+    links: [
+      {
+        url: 'https://github.com/andonynt/linkeefy-app',
+        text: 'repo',
+      },
+    ]
+  },
+]
